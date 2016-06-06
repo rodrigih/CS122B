dde0c9e07367a86ef3e0589acd8044e68ac2959c
rodrigih <rodrigih@uci.edu>
2016-06-06 16:44:53 -0700

    Added files to upload to instances

---
+++ AdvSearchServlet2.java
@@ -0,0 +1,135 @@
+package khtest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpSession;
+
+import Model.*;
+
+public class AdvSearchServlet2 extends HttpServlet {
+	// process
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AdvSearchServlet2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		HttpSession session = request.getSession();
+//        ArrayList<Movie> movieList = new ArrayList<Movie>();
+        ArrayList<Model.Movie> movieList = new ArrayList<Model.Movie>();
+
+		String title = request.getParameter("title");
+		String year = request.getParameter("year");
+		String director = request.getParameter("director");
+		String name = request.getParameter("name");
+
+		String query = "Select * from movies "
+				+ "WHERE title LIKE ? "
+				+ "AND year LIKE ? "
+				+ "AND director LIKE ?"
+				+ " AND id IN "
+				+ "(SELECT movie_id "
+				+ "FROM stars_in_movies a natural JOIN stars b "
+				+ "WHERE a.star_id = b.id AND ";
+
+		try {
+			Connection connection = DBConnection.connectToDatabase();
+			PreparedStatement selectMovie;
+
+			String[] splitNames = name.split(" ");
+			if (splitNames.length <= 1) {
+				query += "(b.first_name LIKE ? OR b.last_name LIKE ?))";
+				selectMovie = connection.prepareStatement(query);
+				selectMovie.setString(1, "%" + title + "%");
+				selectMovie.setString(2, "%" + year + "%");
+				selectMovie.setString(3, "%" + director + "%");
+				selectMovie.setString(4, "%" + name + "%");
+				selectMovie.setString(5, "%" + name + "%");
+
+			} else if (splitNames.length == 2) {
+				query += "(b.first_name LIKE ? OR b.first_name LIKE ? AND b.last_name LIKE ? OR b.last_name LIKE ?))";
+				selectMovie = connection.prepareStatement(query);
+				selectMovie.setString(1, "%" + title + "%");
+				selectMovie.setString(2, "%" + year + "%");
+				selectMovie.setString(3, "%" + director + "%");
+				selectMovie.setString(4, "%" + name + "%");
+				selectMovie.setString(5, "%" + splitNames[0] + "%");
+				selectMovie.setString(6, "%" + splitNames[1] + "%");
+				selectMovie.setString(7, "%" + name + "%");
+
+			} else {
+				query += "(b.first_name LIKE ? OR b.first_name LIKE ?"
+						+ " OR b.first_name LIKE ? OR b.last_name LIKE ? OR b.last_name LIKE ?"
+						+ " OR b.last_name LIKE ?))";
+
+				selectMovie = connection.prepareStatement(query);
+				selectMovie.setString(1, "%" + title + "%");
+				selectMovie.setString(2, "%" + year + "%");
+				selectMovie.setString(3, "%" + director + "%");
+				selectMovie.setString(4, "%" + splitNames[0] + "%");
+				selectMovie.setString(5, "%" + splitNames[0] + splitNames[1] + "%");
+				selectMovie.setString(6, "%" + name + "%");
+				selectMovie.setString(7, "%" + splitNames[2] + "%");
+				selectMovie.setString(8, "%" + splitNames[1] + splitNames[2] + "%");
+				selectMovie.setString(9, "%" + name + "%");
+
+			}
+
+//			System.out.println(selectMovie.toString());
+
+			ResultSet result;
+			result = selectMovie.executeQuery();
+			while (result.next()) {
+				int id = result.getInt("id");
+				String _title = result.getString("title");
+				int _year = result.getInt("year");
+				String _director = result.getString("director");
+				String b_url = result.getString("banner_url");
+				String t_url = result.getString("trailer_url");
+	            Model.Movie movie = new Model.Movie(id,_title,_year,_director,b_url,t_url);
+	            movieList.add(movie);
+//				Movie movie = new Movie(id, _title, _year, _director, b_url, t_url);
+//				movieList.add(movie);
+			}
+			connection.close();
+            Model.MovieList movies = new Model.MovieList(movieList, title+year+director+name);
+            session.setAttribute("movieList",movies);
+            RequestDispatcher rd = request.getRequestDispatcher("/showMovies.jsp");
+            rd.forward(request,response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Search2.java
@@ -0,0 +1,134 @@
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Search2 {
+	static Connection connection = DBConnection.connectToDatabase();
+
+
+	public static ArrayList<Movie> createList(String keywords) {
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		Set<Movie> movieSet = new HashSet<Movie>();
+		try {
+			for (Movie movie : getMoviesByStars(keywords)) {
+				movieSet.add(movie);
+			}
+			for (Movie movie : getMoviesByGenre(keywords)) {
+				movieSet.add(movie);
+			}
+			for (Movie movie : getMoviesByKey(keywords)) {
+				movieSet.add(movie);
+			}
+			for (Movie movie : movieSet) {
+				movies.add(movie);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return movies;
+	}
+
+	private static ArrayList<Movie> getMoviesByStars(String keyword) throws SQLException {
+		String[] splitKeyword = keyword.split(" ");
+		ResultSet result;
+		ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+		String queryStatement = "SELECT * FROM movies WHERE id IN " + "(SELECT movie_id FROM "
+				+ "stars_in_movies a natural JOIN stars b " + "WHERE a.star_id = b.id AND ";
+		PreparedStatement selectMovie;
+
+		if (splitKeyword.length <= 1) {
+			queryStatement += "(b.first_name LIKE ? OR " + "b.last_name LIKE ?))";
+			selectMovie = connection.prepareStatement(queryStatement);
+			selectMovie.setString(1, "%" + keyword + "%");
+			selectMovie.setString(2, "%" + keyword + "%");
+			result = selectMovie.executeQuery();
+		} else if (splitKeyword.length == 2) {
+			queryStatement += "(b.first_name LIKE ? OR " + "b.first_name LIKE ? AND " + "b.last_name LIKE ? OR "
+					+ "b.last_name LIKE ?))";
+			selectMovie = connection.prepareStatement(queryStatement);
+			selectMovie.setString(1, "%" + keyword + "%");
+			selectMovie.setString(2, "%" + splitKeyword[0] + "%");
+			selectMovie.setString(3, "%" + splitKeyword[1] + "%");
+			selectMovie.setString(4, "%" + keyword + "%");
+			result = selectMovie.executeQuery();
+		} else {
+			queryStatement += "(b.first_name LIKE ? OR " + "b.first_name LIKE ? OR " + "b.first_name LIKE ? OR "
+					+ "b.last_name LIKE ? OR " + "b.last_name LIKE ? OR " + "b.last_name LIKE ?))";
+			selectMovie = connection.prepareStatement(queryStatement);
+			selectMovie.setString(1, "%" + splitKeyword[0] + "%");
+			selectMovie.setString(2, "%" + splitKeyword[0] + splitKeyword[1] + "%");
+			selectMovie.setString(3, "%" + keyword + "%");
+			selectMovie.setString(4, "%" + splitKeyword[2] + "%");
+			selectMovie.setString(5, "%" + splitKeyword[1] + splitKeyword[2] + "%");
+			selectMovie.setString(6, "%" + keyword + "%");
+			result = selectMovie.executeQuery();
+		}
+		while (result.next()) {
+			int id = result.getInt("id");
+			String title = result.getString("title");
+			int year = result.getInt("year");
+			String director = result.getString("director");
+			String b_url = result.getString("banner_url");
+			String t_url = result.getString("trailer_url");
+			Movie movie = new Movie(id, title, year, director, b_url, t_url);
+			movieList.add(movie);
+		}
+		return movieList;
+	}
+
+	private static ArrayList<Movie> getMoviesByGenre(String keyword) throws SQLException {
+		ResultSet result;
+		ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+		String query = "SELECT * FROM movies WHERE id IN "
+				+ "(SELECT movie_id FROM genres_in_movies a NATURAL JOIN genres b"
+				+ " WHERE a.genre_id = b.id AND (b.name LIKE ? ))";
+
+		PreparedStatement selectMovie;
+		selectMovie = connection.prepareStatement(query);
+		selectMovie.setString(1, "%" + keyword + "%");
+		result = selectMovie.executeQuery();
+
+		while (result.next()) {
+			int id = result.getInt("id");
+			String title = result.getString("title");
+			int year = result.getInt("year");
+			String director = result.getString("director");
+			String b_url = result.getString("banner_url");
+			String t_url = result.getString("trailer_url");
+			Movie movie = new Movie(id, title, year, director, b_url, t_url);
+			movieList.add(movie);
+		}
+		return movieList;
+	}
+
+	private static ArrayList<Movie> getMoviesByKey(String keyword) throws SQLException {
+		ResultSet result;
+		ArrayList<Movie> movieList = new ArrayList<Movie>();
+
+		String query = "SELECT * FROM movies WHERE " + "title LIKE ? OR " + "director LIKE ?;";
+
+		PreparedStatement selectMovie;
+		selectMovie = connection.prepareStatement(query);
+		selectMovie.setString(1, "%" + keyword + "%");
+		selectMovie.setString(2, "%" + keyword + "%");
+		result = selectMovie.executeQuery();
+
+		while (result.next()) {
+			int id = result.getInt("id");
+			String title = result.getString("title");
+			int year = result.getInt("year");
+			String director = result.getString("director");
+			String b_url = result.getString("banner_url");
+			String t_url = result.getString("trailer_url");
+			Movie movie = new Movie(id, title, year, director, b_url, t_url);
+			movieList.add(movie);
+		}
+		return movieList;
+	}
+
+}
